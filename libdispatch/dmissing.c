fa32253e77ee677bd3963525d187760215f0e63a
Georg Semmler <georg.semmler@giga-infosystems.com>
2025-06-10 14:48:10 +0200

    Remove strlcpy from libdispatch
    It seems like this function was not used somewhere. Having this symbol
    there is causing issues for me a project where I link netcdf and several
    other libraries. One of the other libraries (libpq) also provides a `strlcpy`
    implementation which results in a duplicated symbol error on linking.
    
    Given that netcdf does not seem to actually use this function I figured
    it would be the easiest solution to just remove it here.

--- libdispatch/dmissing.c
+++ libdispatch/dmissing.c
@@ -9,9 +9,8 @@
  * Currently, this defines:
  *   strdup
  *   strcpy
- *   strlcpy
  *   strlcat
-*/
+ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -29,67 +28,20 @@
 /**************************************************/
 
 #ifndef HAVE_STRDUP
-char*
-strdup(const char* s)
-{
-    char* dup;
+char *strdup(const char *s) {
+    char *dup;
     size_t len;
-    if(s == NULL) return NULL;
+    if (s == NULL)
+        return NULL;
     len = strlen(s);
-    dup = (char*)malloc(len+1);
-    memcpy(dup,s,len);
+    dup = (char *)malloc(len + 1);
+    memcpy(dup, s, len);
     dup[len] = '\0';
     return dup;
 }
 #endif
 
-
 #if !defined(_MSC_VER) && !defined(WIN32)
-
-#ifndef HAVE_STRLCPY
-/*
- * Copyright (c) 1998, 2015 Todd C. Miller <Todd.Miller@courtesan.com>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-/*
- * Copy string src to buffer dst of size dsize.  At most dsize-1
- * chars will be copied.  Always NUL terminates (unless dsize == 0).
- * Returns strlen(src); if retval >= dsize, truncation occurred.
- */
-size_t
-strlcpy(char *dst, const char* src, size_t dsize)
-{
-	const char *osrc = src;
-	size_t nleft = dsize;
-
-	/* Copy as many bytes as will fit. */
-	if (nleft != 0) {
-		while (--nleft != 0) {
-			if ((*dst++ = *src++) == '\0')
-				break;
-		}
-	}
-	/* Not enough room in dst, add NUL and traverse rest of src. */
-	if (nleft == 0) {
-		if (dsize != 0)
-			*dst = '\0';		/* NUL-terminate dst */
-		while (*src++)
-			;
-	}
-	return(src - osrc - 1);	/* count does not include NUL */
-}
-#endif
 
 #ifndef HAVE_STRLCAT
 /* strlcat */
@@ -116,32 +68,30 @@ strlcpy(char *dst, const char* src, size_t dsize)
  * Returns strlen(src) + MIN(dsize, strlen(initial dst)).
  * If retval >= dsize, truncation occurred.
  */
-size_t
-nc_strlcat(char* dst, const char* src, size_t dsize)
-{
-	const char *odst = dst;
-	const char *osrc = src;
-	size_t n = dsize;
-	size_t dlen;
+size_t nc_strlcat(char *dst, const char *src, size_t dsize) {
+    const char *odst = dst;
+    const char *osrc = src;
+    size_t n = dsize;
+    size_t dlen;
 
-	/* Find the end of dst and adjust bytes left but don't go past end. */
-	while (n-- != 0 && *dst != '\0')
-		dst++;
-	dlen = dst - odst;
-	n = dsize - dlen;
+    /* Find the end of dst and adjust bytes left but don't go past end. */
+    while (n-- != 0 && *dst != '\0')
+        dst++;
+    dlen = dst - odst;
+    n = dsize - dlen;
 
-	if (n-- == 0)
-		return(dlen + strlen(src));
-	while (*src != '\0') {
-		if (n != 0) {
-			*dst++ = *src;
-			n--;
-		}
-		src++;
-	}
-	*dst = '\0';
+    if (n-- == 0)
+        return (dlen + strlen(src));
+    while (*src != '\0') {
+        if (n != 0) {
+            *dst++ = *src;
+            n--;
+        }
+        src++;
+    }
+    *dst = '\0';
 
-	return(dlen + (src - osrc));	/* count does not include NUL */
+    return (dlen + (src - osrc)); /* count does not include NUL */
 }
 #endif /*!HAVE_STRLCAT*/
 
